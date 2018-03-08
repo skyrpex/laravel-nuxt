c2d8302d4d4f17eca491b643ba365c04875a389b
Cristian Pallarés <cristian@pallares.io>
2018-03-08 11:53:49 +0100

    refactor: move the rendering method apart

--- src/Controllers/NuxtController.php
+++ src/Controllers/NuxtController.php
@@ -7,6 +7,12 @@ use Illuminate\Routing\Controller;
 
 class NuxtController extends Controller
 {
+    /**
+     * Handle the SPA request.
+     *
+     * @param Request $request
+     * @return string
+     */
     public function __invoke(Request $request)
     {
         // If the request expects JSON, it means that
@@ -15,6 +21,16 @@ class NuxtController extends Controller
             abort(404);
         }
 
+        return $this->renderNuxtPage();
+    }
+
+    /**
+     * Render the Nuxt page.
+     *
+     * @return string
+     */
+    protected function renderNuxtPage()
+    {
         // In production, this will display the precompiled nuxt page.
         // In development, this will fetch and display the page from the nuxt's dev server.
         return file_get_contents(config('nuxt.page'));
